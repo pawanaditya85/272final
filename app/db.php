4f7297d3b842ac6e7cf9ca680dad30f7880444ec
EC2 Default User <ec2-user@ip-172-31-4-94.us-east-2.compute.internal>
2024-12-10 23:46:27 +0000

    changes

--- app/db.php
+++ app/db.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'config.php';
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli("db"," user"," rootpassword"," marketplace");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
