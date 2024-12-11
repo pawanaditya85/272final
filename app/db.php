4caac87e6526ebc7c41983df47cf6dac42f9b887
pawanaditya369 <pawanaditya85@gmail.com>
2024-12-10 16:31:47 -0800

    Update db.php

--- app/db.php
+++ app/db.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'config.php';
-$conn = new mysqli("db"," user","password"," marketplace");
+
+$conn = new mysqli("db", "user", "password", "marketplace");
+
+// Debugging connection
+echo $conn->ping() ? "Connected successfully" : "Connection failed";
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
