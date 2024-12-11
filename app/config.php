5d84cc19133ca6132f4f69d5c3ca9145588e3d64
pawanaditya369 <pawanaditya85@gmail.com>
2024-12-10 16:28:29 -0800

    Update config.php

--- app/config.php
+++ app/config.php
@@ -2,6 +2,6 @@
 // Database configuration
 $db_host = "db";          // Use the service name of the db container
 $db_user = "user";        // Matches MYSQL_USER from docker-compose
-$db_pass = "password";    // Matches MYSQL_PASSWORD from docker-compose
+$db_pass = "rootpassword";    // Matches MYSQL_PASSWORD from docker-compose
 $db_name = "marketplace"; // Matches MYSQL_DATABASE from docker-compose
 ?>
